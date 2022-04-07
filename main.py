c9af0864e0345ee2f2ff3f54920d508c49e7fc39
Maciej Kucharski <s20153@pjwstk.edu.pl>
2022-04-07 16:58:17 +0200

    client settings

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import dask.dataframe as dd
 from dask.distributed import Client
+from dask_cloudprovider.gcp import GCPCluster
 import time
 
-
-client = Client("<insert_ip_v4>:8786")
-
-df = dd.read_parquet('data*/new_*.parquet', engine="pyarrow")
-
-start_time = time.time()
-df = df['repo_name'].explode()
-print(df.compute())
-print(f"time: {time.time() - start_time}")
-
-print(client)
+with GCPCluster(on_host_maintenance="STOP", env_vars=dict(EXTRA_PIP_PACKAGES="pyarrow gcsfs blosc lz4"),
+                projectid="bgt-labs-20701",
+                n_workers=3, machine_type="e2-small") as cluster:
+    with Client(cluster) as client:
+        df = dd.read_parquet(
+            ["./data1/new_*.parquet", "./data2/new_*.parquet", "./data3/new_*.parquet", "./data4/new_*.parquet"],
+            engine="pyarrow")
+        start_time = time.time()
+        df = df['repo_name'].explode()
+        print(df.compute())
+        print(f"time: {time.time() - start_time}")
+        print(client)
 
