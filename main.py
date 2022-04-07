c20ae1fcc763aee43c01dd8227a5ac732f324e40
Maciej Kucharski <s20153@pjwstk.edu.pl>
2022-04-07 16:19:10 +0200

    dsa

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ df = dd.read_parquet('data*/new_*.parquet', engine="pyarrow")
 
 start_time = time.time()
 df = df['repo_name'].explode()
-df = df.value_counts(ascending=True)
 print(df.compute())
 print(f"time: {time.time() - start_time}")
 
