ad470db41cb37b703562cceac9a76309dbabf4b3
Maciej Kucharski <s20153@pjwstk.edu.pl>
2022-04-07 17:03:43 +0200

    s

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dask_cloudprovider.gcp import GCPCluster
 import time
 
 with GCPCluster(on_host_maintenance="STOP", env_vars=dict(EXTRA_PIP_PACKAGES="pyarrow gcsfs blosc lz4"),
-                projectid="bgt-labs-20701",
+                projectid="bgt_lab4",
                 n_workers=3, machine_type="e2-small") as cluster:
     with Client(cluster) as client:
         df = dd.read_parquet(
