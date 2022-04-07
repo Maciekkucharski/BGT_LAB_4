53b45fb8394ee56b8ad59ec0eca2e5856545d50a
Maciej Kucharski <s20153@pjwstk.edu.pl>
2022-04-07 16:51:08 +0200

    s

--- worker.sh
+++ worker.sh
@@ -4,8 +4,8 @@ pip install dask[complete]
 pip install pyarrow
 
 git clone https://github.com/Maciekkucharski/BGT_LAB_4.git
-
-mkdir ~/data1, ~/data2, ~/data3, ~/data4
+cd BGT_LAB_4
+mkdir ~/data1
 gsutil -m cp gs://pjwstk-bigdata/*.parquet ./data1/.
 sudo cp -r ~/data1 ~/data2
 sudo cp -r ~/data1 ~/data3
